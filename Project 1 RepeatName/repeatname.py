022c1e4a20e27f14177ad8fba027d3184ee7fc28
Ray Harris <rayrayjoe2006@gmail.com>
2019-10-01 11:58:39 -0700

    Python Project 1 (Repeat Name) py file and pdf instructions.

---
+++ Project 1 RepeatName/repeatname.py
@@ -0,0 +1,15 @@
+# repeatName.py
+
+# Nolaundron 'Ray' Harris
+
+
+def main():
+
+    #Ask the user to input their name.
+    name = (input("Please input your name here: "))
+
+    #Use for loop to print name 5 times.
+    for i in range(5):
+        print(name)
+
+main()
